3c0715da6036e26bfb927bcc548e7c7cbd3e7492
nityanandanantkamal-netizen <nityanand.anantkamal@gmail.com>
2026-01-10 22:42:46 +0530

    Add initial modal schemas for Ecommerce and hospital management

--- modals/hospitalManagement/medical_record.modal.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-const medicalRecordSchema = new mongoose.Schema({}, { timestamps: true });
-
-export default mongoose.model("MedicalRecord", medicalRecordSchema);
